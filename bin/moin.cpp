278bcc0de26f61c07650808de3396639aa431946
Rostislav-Paul Wilhelm <paul.wilhelm@rwth-aachen.de>
2021-02-04 15:30:39 +0100

    ja moin

--- bin/moin.cpp
+++ bin/moin.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
+#include <armadillo>
 
 int main() 
 {
+	arma::mat guenther(10, 10, arma::fill::randu);
 	std::cout << "Ja moin" << std::endl;
+	std::cout << inv(guenther);
+
 	return 0;
 }
