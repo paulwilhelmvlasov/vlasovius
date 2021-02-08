ca8a6919cdc4e1068527c3845eec5f21d686708b
Rostislav-Paul Wilhelm <paul.wilhelm@rwth-aachen.de>
2021-02-08 17:08:37 +0100

    startet writing direct-interpolator. because of compiler issues cannot test code yet

---
+++ vlasovius/interpolators/directInterpolator.cpp
@@ -0,0 +1,73 @@
+#include "directInterpolator.h"
+/*
+ * Copyright (C) 2021 Matthias Kirchhart and Paul Wilhelm
+ *
+ * This file is part of vlasovius.
+ *
+ * vlasovius is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3, or (at your option) any later
+ * version.
+ *
+ * vlasovius is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * vlasovius; see the file COPYING.  If not see http://www.gnu.org/licenses.
+ */
+
+template<typename kernel>
+inline directInterpolator<kernel>::directInterpolator
+(const std::vector<point>& X, const std::vector<double>& f) : interPolPoints(X)
+{
+	if (X.empty()) {
+		throw std::runtime_error("No interpolation-points passed.");
+	}
+
+	arma::mat rkhsMat = computeRKHSMatrix(X);
+	computeCoeffVec(rkhsMat, f);
+}
+
+template<typename kernel>
+double operator(const point& x) const
+{
+	arma::vec v(N);
+
+	#pragma omp parallel for
+	for (long i = 0; i < N; i++) {
+		v(i) = kernel(interPolPoints[i], x);
+	}
+
+	return v.dot(coeff);
+}
+
+template<typename kernel>
+arma::mat vlasovius::interpolators::directInterpolator<kernel>::computeRKHSMatrix
+(const std::vector<point>& X)
+{
+	N = X.size();
+	arma::mat matrix(N, N);
+
+	double norm = kernel(X[0], X[0]);
+
+	#pragma omp parallel for
+	for (long i = 0; i < N; i++) {
+		matrix(i, i) = norm;
+		for (long j = i + 1; j < N; j++) {
+			matrix(i, j) = kernel(X[i], X[j]);
+			matrix(j, i) = matrix(i, j);
+		}
+	}
+
+	return matrix;
+}
+
+template<typename kernel>
+void vlasovius::interpolators::directInterpolator<kernel>::computeCoeffVec
+(const arma::mat& rkhsMat, const std::vector<double>& f)
+{
+	coeff = solve(rkhsMat, f);
+}
+
---
+++ vlasovius/interpolators/directInterpolator.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2021 Matthias Kirchhart and Paul Wilhelm
+ *
+ * This file is part of vlasovius.
+ *
+ * vlasovius is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3, or (at your option) any later
+ * version.
+ *
+ * vlasovius is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * vlasovius; see the file COPYING.  If not see http://www.gnu.org/licenses.
+ */
+#ifndef VLASOVIUS_INTERPOLATORS_DIRECTINTERPOLATOR_H
+#define VLASOVIUS_INTERPOLATORS_DIRECTINTERPOLATOR_H
+
+#include <vector>
+
+namespace vlasovius
+{
+	typedef point = arma::vec; 
+
+	namespace interpolators
+	{
+		template <typename kernel>
+		class directInterpolator
+		{
+		public:
+			directInterpolator(const std::vector<point>& X, const std::vector<double>& f, double sigma = 1.0);
+			
+		public:
+			double operator(const point& x) const;
+
+		private:
+			arma::mat computeRKHSMatrix(const std::vector<point>& X);
+			void computeCoeffVec(const arma::mat& rkhsMat, const std::vector<double>& f);
+
+		private:
+			long N;
+
+			arma::vec coeff;
+			const std::vector<point> interPolPoints; 
+		};
+	}
+
+}
+
+#include <vlasovius/interpolators/direct_interpolators.cpp>
+#endif
