97d4a4b776ed942bf5d952066e7ce41fe0ba7796
Rostislav-Paul Wilhelm <paul.wilhelm@rwth-aachen.de>
2021-02-09 16:01:59 +0100

    direct interpolator and quadtree

---
+++ vlasovius/trees/quadtree.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2021 Matthias Kirchhart and Paul Wilhelm
+ *
+ * This file is part of vlasovius.
+ *
+ * vlasovius is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3, or (at your option) any later
+ * version.
+ *
+ * vlasovius is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * vlasovius; see the file COPYING.  If not see http://www.gnu.org/licenses.
+ */
+
+#include "quadtree.h"
+
+bool vlasovius::trees::bounding_box_2d::pointInsideAABB(const arma::vec2& x, double tol)
+{
+	return norm(x - center, 2) < tol;
+}
