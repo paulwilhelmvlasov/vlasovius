e827cef631f48f88f4de0204297462eddedb63b4
Paul Wilhelm <wilhelm@acom.rwth-aachen.de>
2021-03-12 14:29:06 +0100

    testing with different kerlens and looking at boundary error. The periodic pou is not periodic (huge difference between f(0,v) and f(L,v)

--- bin/test_direct_interpolator.cpp
+++ bin/test_direct_interpolator.cpp
@@ -24,35 +24,50 @@
 
 
 #include <vlasovius/misc/stopwatch.h>
+#include <vlasovius/misc/xv_kernel.h>
 #include <vlasovius/kernels/wendland.h>
 #include <vlasovius/kernels/rbf_kernel.h>
 #include <vlasovius/interpolators/direct_interpolator.h>
 
+double maxwell(double x, double v, double alpha = 0.01, double k = 0.5)
+{
+	return 0.39894228040143267793994 * ( 1 + alpha * std::cos(k * x) )
+			* std::exp( -0.5 * v*v );
+}
+
 int main()
 {
 	constexpr size_t dim { 2 }, k { 4 };
-	constexpr size_t N { 10000 };
+	constexpr size_t N { 2'000 };
 
 	constexpr double tikhonov_mu { 1e-12 };
 	constexpr double twopi { 2*3.1415926535 };
 
+	double L = 4 * M_PI;
+	double v_max = 10.0;
+
 	size_t threads { (size_t) omp_get_max_threads() };
 
 	using wendland_t     = vlasovius::kernels::wendland<dim,k>;
-	using kernel_t       = vlasovius::kernels::rbf_kernel<wendland_t>;
+	//using kernel_t       = vlasovius::kernels::rbf_kernel<wendland_t>;
+	using kernel_t = vlasovius::xv_kernel<4, 4>;
 	using interpolator_t = vlasovius::interpolators::direct_interpolator<kernel_t>;
 
 	arma::mat X( N, 2, arma::fill::randu );
+	X.col(0) *= L;
+	X.col(1) = 2 * v_max * X.col(1)
+			- v_max * arma::vec(N, arma::fill::ones);
 	arma::vec f( N );
 	for ( size_t i = 0; i < N; ++i )
 	{
-		double x = twopi*X(i,0);
-		double y = twopi*X(i,1);
-		f(i) = std::sin(x)*std::sin(y);
+		f(i) = maxwell(X(i,0), X(i,1), 0.01, 0.5);
 	}
 
+	//kernel_t K { wendland_t {}, 4.0 };
+	kernel_t K {4.0, 4.0, L};
+
 	vlasovius::misc::stopwatch clock;
-	interpolator_t sfx { kernel_t {}, X, f, tikhonov_mu, threads };
+	interpolator_t sfx { K, X, f, tikhonov_mu, threads };
 	double elapsed { clock.elapsed() };
 	std::cout << "Time for computing RBF-Approximation: " << elapsed << ".\n";
 	std::cout << "Maximal interpolation error: " << norm(f-sfx(X),"inf") << ".\n";
@@ -62,9 +77,9 @@ int main()
 	for ( size_t i = 0; i <= 100; ++i )
 	for ( size_t j = 0; j <= 100; ++j )
 	{
-		double x = plotX(j + 101*i,0) = i/100.;
-		double y = plotX(j + 101*i,1) = j/100.;
-		plotf_true(j + 101*i) = std::sin(twopi*x)*std::sin(twopi*y);
+		double x = plotX(j + 101*i,0) = L * i/100.;
+		double y = plotX(j + 101*i,1) = 2 * v_max * j/100. - v_max;
+		plotf_true(j + 101*i) = maxwell(x, y, 0.01, 0.5);
 	}
 
 	clock.reset();
@@ -80,10 +95,26 @@ int main()
 		{
 			double x = plotX(j + 101*i,0);
 			double y = plotX(j + 101*i,1);
-			double err  = plotf(j+101*i)-plotf_true(j+101*i);
+			double err  = plotf(j+101*i) - plotf_true(j+101*i);
 			str << x << " " << y << " " << err << std::endl;
 		}
 		str << "\n";
 	}
+
+	std::ofstream str2( "test_boundary.txt" );
+	for ( size_t i = 0; i <= 100; ++i )
+	{
+		double v = 2 * v_max * i / 100. - v_max;
+		double err  = sfx(arma::rowvec{0, v})(0) - sfx(arma::rowvec{L, v})(0);
+		str2 << v << " " << err << std::endl;
+	}
+
+	std::ofstream str3( "test_points.txt" );
+	for ( size_t i = 0; i < N; ++i )
+	{
+		str3 << X(i, 0) << " " << X(i, 1) << std::endl;
+	}
+
+	return 0;
 }
 
--- bin/test_pou_interpolator.cpp
+++ bin/test_pou_interpolator.cpp
@@ -17,43 +17,59 @@
  * vlasovius; see the file COPYING.  If not see http://www.gnu.org/licenses.
  */
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include <iostream>
 #include <armadillo>
 
 #include <vlasovius/misc/stopwatch.h>
+#include <vlasovius/misc/xv_kernel.h>
 #include <vlasovius/kernels/wendland.h>
 #include <vlasovius/kernels/rbf_kernel.h>
 #include <vlasovius/interpolators/pou_interpolator.h>
 #include <vlasovius/interpolators/periodic_pou_interpolator.h>
 
+double maxwell(double x, double v, double alpha = 0.01, double k = 0.5)
+{
+	return 0.39894228040143267793994 * ( 1 + alpha * std::cos(k * x) )
+			* std::exp( -0.5 * v*v );
+}
+
 int main()
 {
 	constexpr size_t dim { 2 }, k { 4 };
-	constexpr size_t N { 1'000'000 };
-	constexpr double tikhonov_mu { 1e-12 };
-	constexpr size_t min_per_box = 100;
+	constexpr size_t N { 1'000 };
+	constexpr double tikhonov_mu { 1e-17 };
+	constexpr size_t min_per_box = 200;
 	constexpr double enlarge = 1.5;
 	constexpr double twopi { 2*3.1415926535 };
+
+	double L = 4 * M_PI;
+	double v_max = 10.0;
 
 	std::cout << "N = " << N << std::endl;
 	std::cout << "min per Box = " << min_per_box << std::endl;
 
 	using wendland_t     = vlasovius::kernels::wendland<dim,k>;
-	using kernel_t       = vlasovius::kernels::rbf_kernel<wendland_t>;
+	//using kernel_t       = vlasovius::kernels::rbf_kernel<wendland_t>;
+	using kernel_t = vlasovius::xv_kernel<4, 4>;
 	using interpolator_t = vlasovius::interpolators::periodic_pou_interpolator<kernel_t>;
 
 	arma::mat X( N, 2, arma::fill::randu );
+	X.col(0) *= L;
+	X.col(1) = 2 * v_max * X.col(1)
+			- v_max * arma::vec(N, arma::fill::ones);
 	arma::vec f( N );
 	for ( size_t i = 0; i < N; ++i )
 	{
-		double x = twopi*X(i,0);
-		double y = twopi*X(i,1);
-		f(i) = std::sin(x)*std::sin(y);
+		f(i) = maxwell(X(i,0), X(i,1), 0.01, 0.5);
 	}
 
-	arma::rowvec bounding_box { 0, 0, 1, 1 };
+	arma::rowvec bounding_box { 0, -v_max, L, v_max };
 
-	kernel_t K { wendland_t {}, 0.5 };
+	//kernel_t K { wendland_t {}, 4.0 };
+	kernel_t K {4.0, 4.0, L};
 	vlasovius::misc::stopwatch clock;
 	interpolator_t sfx { K, X, f, bounding_box, enlarge, min_per_box, tikhonov_mu };
 	double elapsed { clock.elapsed() };
@@ -69,9 +85,9 @@ int main()
 	for ( size_t i = 0; i <= 100; ++i )
 		for ( size_t j = 0; j <= 100; ++j )
 		{
-			double x = plotX(j + 101*i,0) = i/100.;
-			double y = plotX(j + 101*i,1) = j/100.;
-			plotf_true(j + 101*i) = std::sin(twopi*x)*std::sin(twopi*y);
+			double x = plotX(j + 101*i,0) = L * i/100.;
+			double y = plotX(j + 101*i,1) = 2 * v_max * j/100. - v_max;
+			plotf_true(j + 101*i) = maxwell(x, y, 0.01, 0.5);
 		}
 
 	clock.reset();
@@ -87,12 +103,25 @@ int main()
 		{
 			double x = plotX(j + 101*i,0);
 			double y = plotX(j + 101*i,1);
-			double err  = plotf(j+101*i)-plotf_true(j+101*i);
+			double err  = plotf(j+101*i) - plotf_true(j+101*i);
 			str << x << " " << y << " " << err << std::endl;
 		}
 		str << "\n";
 	}
 
+	std::ofstream str2( "test_boundary.txt" );
+	for ( size_t i = 0; i <= 100; ++i )
+	{
+		double v = 2 * v_max * i / 100. - v_max;
+		double err  = sfx(arma::rowvec{0, v})(0) - sfx(arma::rowvec{L, v})(0);
+		str2 << v << " " << err << std::endl;
+	}
+
+	std::ofstream str3( "test_points.txt" );
+	for ( size_t i = 0; i < N; ++i )
+	{
+		str3 << X(i, 0) << " " << X(i, 1) << std::endl;
+	}
 
 	return 0;
 }
